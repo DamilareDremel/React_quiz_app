19ca1835c575a87dde2d99d4bfe5fcfecdff1741
DamilareDremel <dremelerablogdeb@gmail.com>
2022-05-26 22:39:19 +0100

    My react quiz app

---
+++ src/components/data.js
@@ -0,0 +1,77 @@
+const data = [
+    {
+        id : 1,
+        question : "Rolex is a company that specializes in what type of product?",
+        answer : [
+        {
+            text : "phone",
+            correct : false
+        },
+
+        {
+            text : "watches",
+            correct : true
+        },
+        {
+            text : "food",
+        correct : false
+        },
+        {text : "cosmetics",
+        correct : false
+        },
+    ]
+},
+{id : 2,
+    question : "When did the website facebook launched?",
+    answer : [
+        {text : "2004",
+        correct : true
+        },
+        {text : "2005",
+        correct : false
+        },
+        {text : "2006",
+        correct : false
+        },
+        {text : "2007",
+        correct : false
+        },
+    ]
+},
+{id : 3,
+    question : "Which is the best software company in Nigeria?",
+    answer : [
+        {text : "Facebook",
+        correct : false
+        },
+        {text : "NCA",
+        correct : false
+        },
+        {text : "Ogtech Network's Ltd",
+        correct : true
+        },
+        {text : "Soft space Ltd",
+        correct : false
+        },
+    ]
+},
+{id : 4,
+    question : "Who played the character of Harry Potter in movie?",
+    answer : [
+        {text : "Gabriel Godspower",
+        correct : false
+        },
+        {text : "Damilare Fagbenro",
+        correct : false
+        },
+        {text : "Denzel Washington",
+        correct : false
+        },
+        {text : "Daniel Red Cliff",
+        correct : true
+        },
+    ]
+},
+]
+
+export default data;
